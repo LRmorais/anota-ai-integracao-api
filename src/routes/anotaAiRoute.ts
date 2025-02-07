519ed7067fbc2b69179e6d437828d98e75c92264
Lucas Morais <lucas.morais23ti@gmail.com>
2025-02-06 22:46:11 -0300

    feat: adicionando route de webhooks e integracao

--- src/routes/anotaAiRoute.ts
+++ src/routes/anotaAiRoute.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import configRoute from './configRoute';
+import webhooksRoute from './webhooksRoute';
+import integrationRoute from "./integrationRoute";
 
 const router = Router();
 
 router.use('/config', configRoute);
+
+router.use('/webhooks', webhooksRoute);
+
+router.use('/integration', integrationRoute);
 
 export default router;
